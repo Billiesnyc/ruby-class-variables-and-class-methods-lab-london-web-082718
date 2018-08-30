429f00fd147202800c1f11000c62c88e91d5d541
Billie Simmons <billiesnyc@gmail.com>
2018-08-30 15:11:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,15 +29,15 @@ class Song
   
  
   def self.genre_count
-    genre_count = {}
+    genre_hash = {}
     @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
+      if genre_hash[genre]
+        genre_hash[genre] += 1 
       else
-        genre_count[genre] = 1
+        genre_hash[genre] = 1
       end
     end
-    genre_count
+    genre_hash
   end
   
   def self.artist_count 
